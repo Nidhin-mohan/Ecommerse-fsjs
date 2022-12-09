91f5d980cbb2e7c43abbedfe99b6b76f45ca225c
Nidhin-mohan <nidhinmohannidhin@gmail.com>
2022-12-09 12:05:49 +0530

    first commit

---
+++ models/user.schema.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+import AuthRoles from "../utils/authRoles";
+import bcrypt from "bcryptjs"
+import JWT from "jsonwebtoken";
+import crypto from "crypto";
+import config  from "process";
+
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "password is required"],
+      minLength: [8, "password must be at least 8 characters"],
+      select: false,
+    },
+    role: {
+      type: String,
+      enum: Object.values(AuthRoles),
+      default: AuthRoles.USER,
+    },
+    forgotPasswordToken: String,
+    forgotPasswordExpiry: Date,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
+export default mongoose.model("User",userSchema);
