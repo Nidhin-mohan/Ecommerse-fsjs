1887873aaecb4a62211967a78f7717e956d99018
nidhin-mohan <nidhinmohannidhin@gmail.com>
2024-08-03 20:47:27 +0530

    comment razzorpay instace

--- config/razorpay.config.js
+++ config/razorpay.config.js
@@ -1,8 +1,8 @@
 const Razorpay = require("razorpay");
 const config = require("./index.js")
-const razorpay = new Razorpay({
-  key_id: config.RAZORPAY_KEY_ID,
-  key_secret: config.RAZORPAY_SECRET,
-});
+// const razorpay = new Razorpay({
+//   key_id: config.RAZORPAY_KEY_ID,
+//   key_secret: config.RAZORPAY_SECRET,
+// });
 
-module.exports = razorpay;
+// module.exports = razorpay;
